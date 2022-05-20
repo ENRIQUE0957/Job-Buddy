0a77b65bd6140d87b57921c2d02ae395e9f8b7a0
enrique cipriano <76191555+ENRIQUE0957@users.noreply.github.com>
2022-05-20 15:03:21 -0500

    creating the header

---
+++ my-app/src/HOMEPAGE/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () =>{
+return(
+
+<div className = "Main-header">
+
+
+<ul className = "list">
+<li className = "title"><h2>job buddy</h2></li>
+<li class = "find-jobs">Find jobs</li>
+<li className = "reviews">Reviews</li>
+<li><button className = "button">Sign Up</button></li>     
+
+    
+</ul> 
+
+</div>
+
+    
+)
+}
+export default Header
