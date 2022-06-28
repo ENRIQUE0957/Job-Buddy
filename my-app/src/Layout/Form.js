1bbf9bc88ccb4fa160d2a91d7fd452332c7b21bd
enrique cipriano <76191555+ENRIQUE0957@users.noreply.github.com>
2022-06-28 14:49:30 -0500

    error validation for the form

--- my-app/src/Layout/Form.js
+++ my-app/src/Layout/Form.js
@@ -2,48 +2,101 @@ import React, { useReducer, useState, useEffect, useRef } from "react";
 import classes from "./Form.module.css"
 import ErrorModal from "../UI/ErrorModal";
 
+const zipCodeReducer = (state,action) =>{
+    if(action.type === "USER-ZIPCODE"){
+        return{value:action.val,isValid:action.val.length>=5}
+    }
+    if(action.type === "ZipCode-BLUR"){
+        return{value:state.value,isValid:state.value.length>=5}
+    }
+    return{value:state,isValid:false};
+}
 
-
-
+const jobReducer = (state,action)=>{
+    if(action.type === "JOB-DESCRIPTION"){
+        return{value:action.val,isValid:action.val.lenght>0}
+    }
+    if(action.type === "BLUR"){
+        return{value:state.value,isValid:state.value.length>0}
+    }
+    return{value:state,isValid:false}
+}
 
 const Form = props => {
+    const[formisValid,setFormisValid]=useState(false)
     const [inputZipCode, updateInputZipcode] = useState('')
     const [inputJob, updateInputJob] = useState('')
     const locationRef = useRef()
     const [erroModal, updateErorrModal] = useState(true)
+    
+    const [zipCodeInput,dispatchZipcode]=useReducer(zipCodeReducer,({value:'',isValid:null}))
+    const [jobInput,dispatchJob]=useReducer(jobReducer,({value:'',isValid:null}))
+    const{isValid,zipCodeisValid} = zipCodeInput
+    const{isValid1,jobInputisValid} = jobInput
 
+    useEffect(()=>{
+    const indentifier = setTimeout(()=>{
+        console.log('checking form validity')
+        setFormisValid(
+        zipCodeisValid && jobInputisValid
+        )
+    
+        
+        
 
-
+    },500);
+    return () =>{
+        console.log('cleaned up')
+        clearTimeout(indentifier)
+    }
+    },[zipCodeisValid])
 
 
 
     //event listener  for the zipcode 
     const locationHandler = (event) => {
-        
-        console.log(event.target.value)
-        updateInputZipcode(event.target.value)
-             
-        
+      dispatchZipcode({val:event.target.value,type:"USER-ZIPCODE"})
+       
+      
+       //to handle state for formisValid      
+        setFormisValid(event.target.value.length >=5)
     }
     //event listener for the job-description 
 
-
+    const validateZipcode = (event)=>{
+        dispatchJob({type:"ZipCode-BLUR"})
+    }
     //function to handle location event
     const jobHandler = (event) => {
-        updateInputJob(event.target.value)
+        dispatchJob({val:event.target.value,type:"JOB-DESCRIPTION"})
         
-
+        
+        setFormisValid(event.target.value.length > 0)
     }
-    
+    const validateJobLocation = (event)=>{
+        dispatchJob({type:"BLUR"})
+    }
+    //function to test the geogle API
+    async function geogleAPI(){
+       const response =  await fetch("https://www.themuse.com/api/public/jobs?company=Amazon&category=Software%20Engineering&level=Entry%20Level&level=Internship&page=1")
+       const data =await response.json()
+       console.log(data)
+    }
     const submitHandler = event => {
         event.preventDefault()
-        if(inputZipCode.length >= 5 && inputJob.length >1 ){
+       
+        console.log(zipCodeInput)
+        console.log(jobInput)
+        
+        //updateErorrModal(jobInput.value.length>0 && zipCodeInput.value.length>=5)
+        /*event.preventDefault()
+        if(inputZipCode.length >= 5 && inputJob.length >0 ){
             props.inputHandle(inputZipCode,inputJob) 
-            updateErorrModal(true)
+            updateErorrModal(false)
             
             
         }else{
-            updateErorrModal(false)
+            updateErorrModal(true)
         }
         console.log(inputJob.length)
        
@@ -51,7 +104,7 @@ const Form = props => {
         updateInputJob('')
         updateInputZipcode('')
 
-
+        */
             
 
 
@@ -71,12 +124,16 @@ const Form = props => {
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <h2>Search Jobs</h2>
+            <div >
             <label className={classes.label} htmlFor="location">location</label>
-            <input value={inputZipCode} onChange={locationHandler} className={classes.input} id="location"></input>
-            {!erroModal &&<ErrorModal >Not a valid response</ErrorModal>}
+            <input value ={zipCodeInput.value} className = {`${formisValid === false?classes.invalid:classes.inputBox}`} onChange={locationHandler} onBlur={validateZipcode} id="location"></input>
+            </div>
+            {!erroModal&&<ErrorModal >Not a valid response</ErrorModal>}
+            
             <label className={classes.label} htmlFor="job">Job-Description</label>
-            <input value={inputJob} onChange={jobHandler} className={classes.input} id="job"></input>
-            <button>submit</button>
+            <input value = {inputJob.value} type="text"  className = {`${formisValid === false?classes.invalid:classes.inputBox}`}onChange={jobHandler} onBlur = {validateJobLocation} id="job"></input>
+            
+            <button className={`${formisValid === false?classes.disabled:classes.input}`} onClick={geogleAPI}>submit</button>
         </form>
 
     )
